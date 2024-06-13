9b20586f572632add8a21ddea0aa946c71a0652c
Fabian Palacios <fabianpc@gmail.com>
2024-06-13 00:22:35 -0500

    some ui to play with

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,16 +7,20 @@ class Property(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=True, default=fields.date.today())
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Type',
-        selection=[('N', 'North'), ('S', 'South'), ('E', 'East'), ('W', 'West')],
-        help="Type is used to separate Leads and Opportunities")
+        string='Garden orientation',
+        selection=[('N', 'North'), ('S', 'South'), ('E', 'East'), ('W', 'West')])
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string='Status',
+        selection=[('1', 'New'), ('2', 'Offer Received'), ('3', 'Offer Accepted'), ('4', 'Sold'), ('5', 'Canceled')],
+        default='1')
